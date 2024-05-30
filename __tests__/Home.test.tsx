8523c25ec44328fbf2186a5dd587d961bf509984
pritom <pritomdip010@gmail.com>
2024-05-30 15:44:18 +0100

    Next boilerplate with jest and testing library

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+
+test("Home", () => {
+  render(<Home />);
+  const home = screen.getByText(/pri/i);
+  expect(home).toBeInTheDocument();
+});
